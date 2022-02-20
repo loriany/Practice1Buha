e95a1e30e3ec0b881daf2bb37bdf92864152f96d
Yana Buha <yana.buga129@gmail.com>
2022-02-20 06:03:49 +0200

    Deleted unnecessary usings

--- ViewModels/DateInputViewModel.cs
+++ ViewModels/DateInputViewModel.cs
@@ -4,10 +4,6 @@ using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Practice1Buha.ViewModels
 {
--- Views/DateInputView.xaml.cs
+++ Views/DateInputView.xaml.cs
@@ -1,18 +1,5 @@
 ﻿using Practice1Buha.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Practice1Buha.Views
 {
