8e1a220ff023201984981d1d5ca1db9574384bdd
Yana Buha <yana.buga129@gmail.com>
2022-02-20 04:37:03 +0200

    Created Model

---
+++ Models/DateOfBirth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1Buha.Models
+{
+    class DateOfBirth
+    {
+        private DateTime _date;
+        public DateTime Date
+        {
+            get
+            {
+                return _date;
+            }
+            set
+            {
+                _date = value;
+            }
+        }
+    }
+}
