cb63c0be3f09d054cdbfc7489fcc4bb5c8be2c87
Yana Buha <yana.buga129@gmail.com>
2022-02-20 04:32:00 +0200

    Created View

---
+++ Views/DateInputView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practice1Buha.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для DateInputView.xaml
+    /// </summary>
+    public partial class DateInputView : UserControl
+    {
+        public DateInputView()
+        {
+            InitializeComponent();
+        }
+    }
+}
